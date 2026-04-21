1aa5da1d0fab2c462939d73972d97f0ad6ed42c7
Vladyslav Toros <vladtoros96@gmail.com>
2026-04-21 12:19:13 +0300

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,55 @@
+pipeline {
+    agent any
+
+    environment {
+        TF_HOME = tool 'terraform'
+        ANSIBLE_HOME = tool 'ansible'
+        SSH_CRED_ID = 'lab7' 
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                sh "${TF_HOME}/terraform init"
+                sh "${TF_HOME}/terraform apply -auto-approve"
+
+                script {
+                    env.VM_IP = sh(script: "${TF_HOME}/terraform output -raw vm_ip", returnStdout: true).trim()
+
+                    if (!env.VM_IP) {
+                        error "IP address not found! Terraform output is empty."
+                    }
+                    echo "Successfully retrieved VM IP: ${env.VM_IP}"
+                }
+            }
+        }
+
+        stage('Ansible Deploy') { // ТЕПЕР ВІН ВСЕРЕДИНІ stages
+            steps {
+                sshagent([SSH_CRED_ID]) {
+                    sh """
+                    ${ANSIBLE_HOME}/ansible-playbook -i '${env.VM_IP},' \
+                    -u toros \
+                    --extra-vars "ansible_ssh_common_args='-o StrictHostKeyChecking=no'" \
+                    playbook.yml
+                    """
+                }
+            }
+        }
+    }
+    post {
+        failure {
+            echo "Deployment failed. Destroying infrastructure..."
+            sh "${TF_HOME}/terraform destroy -auto-approve"
+        }
+        always {
+            cleanWs()
+        }
+    }
+}
---
+++ main.tf
@@ -0,0 +1,152 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.9.7"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+# mage
+resource "libvirt_volume" "ubuntu" {
+  name = "ubuntu-22.04-base.qcow2"
+  pool = "default"
+  target = {
+    format = { type = "qcow2" }
+  }
+  create = {
+    content = {
+      url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+    }
+  }
+}
+
+# VM disk
+resource "libvirt_volume" "vm_disk" {
+  name     = "vm-system-disk.qcow2"
+  pool     = "default"
+  capacity = 10737418240 
+  target   = { format = { type = "qcow2" } }
+  backing_store = {
+    path   = libvirt_volume.ubuntu.path
+    format = { type = "qcow2" }
+  }
+}
+
+# Cloud-Init
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name = "commoninit.iso"
+
+  user_data = <<-EOF
+#cloud-config
+users:
+  - name: toros
+    sudo: "ALL=(ALL) NOPASSWD:ALL"
+    shell: /bin/bash
+    lock_passwd: false
+    ssh_authorized_keys:
+      - "${file("/home/toros/.ssh/id_rsa.pub")}"
+
+chpasswd:
+  list: |
+    toros:1vlad1t2
+  expire: false
+
+ssh_pwauth: true
+
+packages:
+  - openssh-server
+
+runcmd:
+  - [ sed, -i, 's/PasswordAuthentication no/PasswordAuthentication yes/g', /etc/ssh/sshd_config ]
+  - [ systemctl, restart, ssh ]
+EOF
+
+  meta_data = <<-EOF
+instance-id: vm-001
+local-hostname: vm
+EOF
+}
+
+resource "libvirt_volume" "commoninit_volume" {
+  name   = "commoninit-vol.iso"
+  pool   = "default"
+  create = {
+    content = {
+      url = libvirt_cloudinit_disk.commoninit.path
+    }
+  }
+}
+
+resource "libvirt_domain" "app_server" {
+  name        = "vm"
+  memory      = 2048
+  memory_unit = "MiB"
+  vcpu        = 2
+  type        = "kvm"
+  running     = true
+
+  os = {
+    type         = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
+  }
+
+  devices = {
+    disks = [
+      {
+        source = {
+          volume = {
+            pool   = libvirt_volume.vm_disk.pool
+            volume = libvirt_volume.vm_disk.name
+          }
+        }
+        driver = { type = "qcow2" }
+        target = { dev = "vda", bus = "virtio" }
+      },
+      {
+        device = "cdrom" 
+        source = {
+          volume = {
+            pool   = libvirt_volume.commoninit_volume.pool
+            volume = libvirt_volume.commoninit_volume.name
+          }
+        }
+        target = { dev = "sda", bus = "sata" }
+      }
+    ]
+
+    interfaces = [
+      {
+        source = {
+          network = { network = "default" }
+        }
+        model = { type = "virtio" }
+        wait_for_ip = {
+          timeout = 300
+          source  = "lease"
+        }
+      }
+    ]
+
+    consoles = [
+      {
+        type = "pty"
+        target = { type = "serial", port = "0" }
+      }
+    ]
+  }
+}
+
+data "libvirt_domain_interface_addresses" "app_server_ip" {
+  domain = libvirt_domain.app_server.name
+  source = "lease"
+}
+
+output "vm_ip" {
+  value = data.libvirt_domain_interface_addresses.app_server_ip.interfaces[0].addrs[0].addr 
+}
